da75e9a612df94c844ac2906efe7173dc2ad4d07
echamale <echamale-2022222@kinal.edu.gt>
2024-05-29 19:41:46 -0600

    finished service model

---
+++ src/service/service.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const ServiceSchema = mongoose.Schema({
+    nameService: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
